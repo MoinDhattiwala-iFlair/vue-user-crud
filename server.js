179f986904ba6a360dcf1621f0d2388de4bcabbb
Moin Dhattiwala <moin.dhattiwala@iflair.com>
2021-02-08 15:57:39 +0530

    Solved url refresh error

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var express = require('express')
 var serveStatic = require('serve-static')
 var path = require("path")
+const history = require('connect-history-api-fallback');
 
 const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+app.use(history({
+    disableDotRule: true,
+    verbose: true
+}));
+
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+app.get('/', function (req, res) {
+    res.render(path.join(__dirname + '/dist/index.html'));
+});
 
 const port = process.env.PORT || 8080
 
